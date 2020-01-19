2b57652f58fba596a6030f54e60aafcb461138f0
Vasil Vanchuk <v.vanchuk@tut.by>
2020-01-19 18:27:03 +0200

    Basic routes and raw templates

--- app.py
+++ app.py
@@ -4,8 +4,28 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'index'
+
+@app.route('/goals/')
+def goals():
+    return '/goals/'
+
+@app.route('/profiles/<id>/')
+def profile():
+    return 'Profile'
+
+@app.route('/search')
+def search():
+    return 'Search'
+
+@app.route('/request/')
+def request():
+    return 'request'
+
+@app.route('/booking/<id>/')
+def booking():
+    return 'booking'
 
 
 if __name__ == '__main__':
