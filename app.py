460a9b573512289045f06df180b16a572836abb9
Vasil Vanchuk <v.vanchuk@tut.by>
2020-01-19 18:46:09 +0200

    Add dependeinces`

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.j2')
+
 
 @app.route('/goals/')
 def goals():
-    return '/goals/'
+    return render_template('goal.j2')
+
 
 @app.route('/profiles/<id>/')
 def profile():
-    return 'Profile'
+    return render_template('profile.j2')
+
 
 @app.route('/search')
 def search():
     return 'Search'
 
+
 @app.route('/request/')
 def request():
     return 'request'
 
+
 @app.route('/booking/<id>/')
 def booking():
-    return 'booking'
+    return render_template('booking.j2')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return (
+        render_template(
+            "404.j2", title="404"
+        ),
+        404,
+    )
 
 
 if __name__ == '__main__':
